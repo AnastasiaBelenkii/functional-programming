93a4173c0be825c9a8caade2bff4aa619aa9caf5
Anastasia Belenkii <anastasia@belenky.net>
2022-10-17 03:18:35 +0000

    solved first exercise

--- src/exercises/Eq01.ts
+++ src/exercises/Eq01.ts
@@ -1,22 +1,34 @@
 /**
- * Definire una istanza di `Eq` per `ReadonlyArray`
- */
-import { Eq } from 'fp-ts/Eq'
-import * as N from 'fp-ts/number'
+* Definire una istanza di `Eq` per `ReadonlyArray`
+*/
+import { Eq } from 'https://deno.land/x/fp_ts@v2.11.4/Eq.ts'
+import * as N from 'https://deno.land/x/fp_ts@v2.11.4/number.ts'
 
-declare const getEq: <A>(E: Eq<A>) => Eq<ReadonlyArray<A>>
+
+
+export function getEq<A>(E: Eq<A>): Eq<ReadonlyArray<A>> {
+    return {
+        equals: (first: ReadonlyArray<A>, second: ReadonlyArray<A>) => (    (first.length == second.length) 
+                                                                                && first.every(
+                                                                                        (x, i) => E.equals(x, second[i])
+                                                                                    )
+                                                                        )
+    }
+}
 
 // ------------------------------------
 // tests
 // ------------------------------------
 
-import * as assert from 'assert'
+import {assertStrictEquals} from 'https://deno.land/std@0.159.0/testing/asserts.ts'
 
 const E = getEq(N.Eq)
 
 const as: ReadonlyArray<number> = [1, 2, 3]
 
-assert.deepStrictEqual(E.equals(as, [1]), false)
-assert.deepStrictEqual(E.equals(as, [1, 2]), false)
-assert.deepStrictEqual(E.equals(as, [1, 2, 3, 4]), false)
-assert.deepStrictEqual(E.equals(as, [1, 2, 3]), true)
+Deno.test("getEq test", () => {
+    assertStrictEquals(E.equals(as, [1]), false)
+    assertStrictEquals(E.equals(as, [1, 2]), false)
+    assertStrictEquals(E.equals(as, [1, 2, 3, 4]), false)
+    assertStrictEquals(E.equals(as, [1, 2, 3]), true)
+});
